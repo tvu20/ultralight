776d03d4d6d23e83f378e23b09584906c37251eb
Giao Vu Dinh <tvudinh20@gmail.com>
2024-02-28 00:44:44 -0500

    library home page

--- css-library/data.js
+++ css-library/data.js
@@ -1,10 +1,97 @@
 const data = [
   {
-    name: "Avalon",
-    status: "owned",
+    name: "Danganronpa: Trigger Happy Havoc",
+    status: "finished",
+    img: "https://i.pinimg.com/originals/7e/7d/25/7e7d2544563430b3bf588d8904aaf097.jpg",
   },
   {
-    name: "Hunger Games",
-    status: "wish",
+    name: "Phoenix Wright Ace Attorney Trilogy",
+    status: "finished",
+    img: "https://i.ebayimg.com/images/g/RqAAAOSw14xWJC2x/s-l1200.webp",
+  },
+  {
+    name: "The Great Ace Attorney Chronicles",
+    status: "finished",
+    img: "https://image.api.playstation.com/vulcan/ap/rnd/202104/1207/ZsL7n15pNYaFoRKOCb9nxfhS.jpg",
+  },
+  {
+    name: "Persona 5 Royal",
+    status: "wishlist",
+    img: "https://m.media-amazon.com/images/I/81--2UBmfgL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "Persona 3 Reload",
+    status: "wishlist",
+    img: "https://e.snmc.io/lk/l/x/60cfc5e9b162026217e288779aaf2d0f/11640738",
+  },
+  {
+    name: "Undertale",
+    status: "finished",
+    img: "https://m.media-amazon.com/images/M/MV5BNzkwZDliNGEtMDNkNi00ODcxLWI1N2UtNDE1NmZlM2QyMTY4XkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_.jpg",
+  },
+  {
+    name: "Deltarune",
+    status: "playing",
+    img: "https://i.redd.it/0hl68gt6i0p71.png",
+  },
+  {
+    name: "Triangle Strategy",
+    status: "finished",
+    img: "https://m.media-amazon.com/images/M/MV5BMTQ4N2E0M2EtNjRkYy00NjI2LTgwNmItYzJhNTBiMDk0NDVlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_.jpg",
+  },
+  {
+    name: "Pokemon Sun",
+    status: "wishlist",
+    img: "https://upload.wikimedia.org/wikipedia/en/b/ba/Pokemon_Sun_Boxart.jpg",
+  },
+  {
+    name: "Pokemon Shield",
+    status: "finished",
+    img: "https://m.media-amazon.com/images/I/71lz62-F84L._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "Fire Emblem Three Houses",
+    status: "finished",
+    img: "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000007606/c499fdc86779ca95e61daed1f94288a245d196360d278138e56d44097d1a3878",
+  },
+  {
+    name: "Breath of the Wild",
+    status: "finished",
+    img: "https://m.media-amazon.com/images/I/81KGsbq8ekL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "Tears of the Kingdom",
+    status: "finished",
+    img: "https://m.media-amazon.com/images/I/81uht0xQ-YL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "Omori",
+    status: "playing",
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Omori_cover.jpg/220px-Omori_cover.jpg",
+  },
+  {
+    name: "Disco Elysium",
+    status: "playing",
+    img: "https://cdn1.epicgames.com/ff52981b1d9947978153c7a7f8bc6d90/offer/EGS_DiscoElysiumTheFinalCut_ZAUM_S6-1200x1600-486d4da970eede7364b9650d63900bad.jpg",
+  },
+  {
+    name: "Hades",
+    status: "finished",
+    img: "https://www.rpgfan.com/wp-content/uploads/2020/08/Hades-Cover-Art.jpg",
+  },
+  {
+    name: "Stardew Valley",
+    status: "playing",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAkTvqNvPZdF1XMMo3r1H9qZJdFyEgcoJIpOQOXRZ8Rg&s",
+  },
+  {
+    name: "Baldur's Gate 3",
+    status: "wishlist",
+    img: "https://preview.redd.it/which-cover-is-better-v0-kukkaczhsccb1.jpg?width=600&format=pjpg&auto=webp&s=8408aced932510b238c4d6d006eb09503b56e86d",
+  },
+  {
+    name: "The Witcher 3",
+    status: "wishlist",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/0c/Witcher_3_cover_art.jpg",
   },
 ];
